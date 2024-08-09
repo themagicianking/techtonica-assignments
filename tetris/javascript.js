7d2e50d1f0a3c54be1f90d24e1b93927785965ed
Thane <tmoonder@gmail.com>
2024-08-09 14:15:11 -0400

    Refactored the drop row function into pullPiecesIntoRow, which now works as intended. Added CSS dynamically to display the grid and drop pieces on a timer.

--- tetris/javascript.js
+++ tetris/javascript.js
@@ -10,6 +10,10 @@ class Shape {
   ];
   color = "blue";
 
+  updateLocation() {
+    // tells the shape where it is
+  }
+
   // the shift methods are for translating the coordinates of the shape from left to right; i want to use this to translate player input
   shiftLeft() {
     this.coordinates.map((item) => (item.x = item.x - 1));
@@ -29,7 +33,7 @@ class Shape {
 class Cell {
   empty = true;
   color = "none";
-  fixed = false;
+  // fixed = false;
 
   constructor(x, y) {
     this.x = x;
@@ -62,7 +66,7 @@ class Grid {
           xIndices.push(index);
         }
       });
-      xIndices.forEach((index) => spaces.push(new Cell(index, yIndex)));
+      xIndices.forEach((index) => spaces.push(this.grid[yIndex][index]));
     });
 
     return spaces;
@@ -98,57 +102,26 @@ class Grid {
     this.grid[y][x].color = "none";
   }
 
+  // drops a cell down into an empty space below it
+  dropCellIntoEmpty(x, y, color) {
+    this.fillSpace(x, y + 1, color);
+  }
+
   // looks at any given row of the grid and converts it to what it "should" be on the next tick; ie, it checks to see if there are any pieces above and drops them into itself. still need to get pieces to stay if the row is at the bottom of the grid.
-  dropRow(rowNum) {
-    // refactoring this code by column one at a time instead of row.
-    // IF THE SPACE IS NOT FIXED:
-    // case one: the space is empty
-    // if there is an occupied space above, the space is now full
-    // if not, remain empty
-    // case two: the space is full
-    // if the space below is empty, the space is now empty
-    // if not, remain full and the space is now fixed
-    // let row = this.grid[rowNum];
-    // row.forEach((cell) => {
-
-    //   );
-    //   if (!cell.fixed) {
-    //     if (cell.empty && hasFullCellAbove) {
-    //       this.fillSpace(cell.x, cell.y, cell.color);
-    //       if (hasFullCellBelow) {
-    //         if (
-    //           this.grid[cell.belowCellLocation[1]][cell.belowCellLocation[0]]
-    //             .fixed ||
-    //           cell.y === 19
-    //         ) {
-    //           cell.fixed = true;
-    //         }
-    //       }
-    //     }
-    //     if (!cell.empty && !hasFullCellBelow) {
-    //       this.emptySpace(cell.x, cell.y);
-    //     }
-    //   }
-    // });
-    // refactoring AGAIN: CHECK TO SEE WHAT'S DROPPING DOWN, drop it, then pull down
-    let row = this.grid[rowNum];
-
-    row.forEach((cell) => {
-      let hasFullCellBelow = this.occupiedSpacesCoordinates.find(
-        (coordinates) =>
-          JSON.stringify(coordinates) === JSON.stringify(cell.belowCellLocation)
-      );
-      let cellBelow;
-
-      hasFullCellBelow
-        ? (cellBelow =
-            this.grid[cell.belowCellLocation[1]][cell.belowCellLocation[0]])
-        : (cellBelow = false);
-
-      let hasLanded = cellBelow.fixed || rowNum === 19;
-      
-      if (!cell.empty && hasLanded) {
-        cell.fixed = true;
+  pullPiecesIntoRow(rowNum) {
+    // check to see what spaces are dropping down and drop them.
+    let rowAbove = this.grid[rowNum - 1];
+    rowAbove.forEach((cellAbove) => {
+      if (cellAbove.empty) {
+        return;
+      } else {
+        let cell = this.grid[rowNum][cellAbove.x];
+        if (cell.empty) {
+          this.fillSpace(cell.x, cell.y, cellAbove.color);
+          console.log(cell.color);
+          this.emptySpace(cellAbove.x, cellAbove.y);
+        } else {
+        }
       }
     });
   }
@@ -176,35 +149,79 @@ function printGrid(grid) {
 
 // dropAllPieces drops every row in the grid once
 function dropAllPieces(grid) {
-  for (let i = grid.grid.length - 1; i >= 0; i--) {
-    grid.dropRow(i);
+  for (let i = grid.grid.length - 1; i >= 1; i--) {
+    grid.pullPiecesIntoRow(i);
   }
 }
 
 let testGrid = new Grid();
-printGrid(testGrid);
+// printGrid(testGrid);
 let testSquare = new Shape();
 insertShape(testSquare, testGrid);
-printGrid(testGrid);
+console.log(testGrid.occupiedSpaces);
+// printGrid(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
+// dropAllPieces(testGrid);
 dropAllPieces(testGrid);
 // dropAllPieces(testGrid);
 // dropAllPieces(testGrid);
+dropAllPieces(testGrid);
 // dropAllPieces(testGrid);
+dropAllPieces(testGrid);
 // dropAllPieces(testGrid);
+dropAllPieces(testGrid);
 // dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-// dropAllPieces(testGrid);
-printGrid(testGrid);
+// printGrid(testGrid);
+
+// visualizing grid on the page
+function createCSSGrid() {
+  const gridElement = document.getElementById("puzzle-container");
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 10; x++) {
+      let cell = document.createElement("div");
+      cell.id = `${x}, ${y}`;
+      cell.classList.add("cell");
+      gridElement.appendChild(cell);
+    }
+  }
+}
+
+createCSSGrid();
+
+// updating the css
+function updateCSSGrid(gridState) {
+  gridState.forEach((row) => {
+    row.forEach((cell) => {
+      // console.log(cell.x, cell.y);
+      let cssCell = document.getElementById(`${cell.x}, ${cell.y}`);
+      if (cell.empty) {
+        cssCell.style.backgroundColor = "inherit";
+      } else {
+        console.log("the cell is full");
+        cssCell.style.backgroundColor = `${cell.color}`;
+      }
+    });
+  });
+}
+
+function performTick() {
+  dropAllPieces(testGrid);
+  updateCSSGrid(testGrid.grid);
+}
+
+window.setInterval(performTick, 500);
