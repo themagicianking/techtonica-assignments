09bb5f317e2e57b7039a02764fe13d46b1a69f7c
Thane <tmoonder@gmail.com>
2024-08-15 22:41:29 -0400

    Created a topfivescores component that renders five scores, currently hardcoded.

--- tetris-in-react/src/main.jsx
+++ tetris-in-react/src/main.jsx
@@ -1,5 +1,7 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
-import './index.css'
+import { render } from "preact";
+// import { App } from "./app.jsx";
+import { TopFiveScores } from "./topfivescores.jsx";
+import "./index.css";
 
-render(<App />, document.getElementById('app'))
+// render(<App />, document.getElementById("app"));
+render(<TopFiveScores />, document.getElementById("top-five-scores"));
