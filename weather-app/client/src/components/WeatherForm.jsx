656e77d04e253ef0cc59cc0039b88e4cdd723c0d
Thane <tmoonder@gmail.com>
2024-08-27 17:48:34 -0400

    Added very simple weather form.

---
+++ weather-app/client/src/components/WeatherForm.jsx
@@ -0,0 +1,13 @@
+function WeatherForm() {
+  return (
+    <>
+      <form>
+        <label for="city">Destination</label>
+        <input id="city"></input>
+        <button type="submit">Take me there</button>
+      </form>
+    </>
+  );
+}
+
+export default WeatherForm;
