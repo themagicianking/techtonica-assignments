bf3794fe4404d5c24258c099f87d55ae07cf4811
Thane <tmoonder@gmail.com>
2024-08-27 19:59:56 -0400

    Added goals to the README.

--- weather-app/client/src/components/WeatherForm.jsx
+++ weather-app/client/src/components/WeatherForm.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 function WeatherForm() {
+  const [userCity, setUserCity] = useState("");
+  const [cityData, setCityData] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUserCity(e.target.value);
+    fetchCityData();
+  }
+
+  function handleChange(e) {
+    setUserCity(e.target.value);
+  }
+
+  async function fetchCityData() {
+    await fetch(`http://localhost:5000/weather?cityName=${userCity}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((body) => {
+        setCityData(JSON.stringify(body.data));
+      });
+  }
+
   return (
     <>
-      <form>
-        <label for="city">Destination</label>
-        <input id="city"></input>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="city">Destination</label>
+        <input value={userCity || ""} onChange={handleChange} id="city"></input>
         <button type="submit">Take me there</button>
       </form>
+      <p>{userCity}</p>
+      <p>{cityData}</p>
     </>
   );
 }
