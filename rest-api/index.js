5ec5d7f0ab14f5c1383d8e4fda401db653b9a4d8
Thane <tmoonder@gmail.com>
2024-08-26 23:02:06 -0400

    Refactored first two get requests to work with database.

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,43 +1,59 @@
-import episodes from "./episodes.js";
 import express from "express";
-
 const app = express();
-
 app.use(express.json());
 
-app.get("/rest-api/episodes", (req, res) => {
-  res.json(episodes);
+// should be in .env file
+import pkg from "pg";
+const { Pool } = pkg;
+const pool = new Pool({
+  user: "tpl1122_1",
+  host: "localhost",
+  database: "nvepisodes",
+  password: "password",
 });
 
-app.get("/rest-api/episode-item", (req, res) => {
-  const index = parseInt(req.query.id);
-  res.json(episodes[index]);
+// getting the whole episode list
+app.get("/rest-api/episode-list", async (req, res) => {
+  const database = await pool.connect();
+  const episodes = await database.query("SELECT * FROM episodes");
+  database.release();
+  res.json(episodes.rows);
 });
 
-app.post("/rest-api/episodes", (req, res) => {
+// getting individual episodes by episode number
+app.get("/rest-api/episode", async (req, res) => {
+  const index = parseInt(req.query.episodeNumber);
+  const database = await pool.connect();
+  const episode = await database.query(`SELECT * FROM episodes WHERE episodeNumber = '${index}'`);
+  database.release();
+  res.json(episode.rows);
+});
+
+app.post("/rest-api/episodes", async (req, res) => {
   const newEpisode = {
-    id: req.body.id,
     episodeNumber: req.body.episodeNumber,
-    releaseDate: req.body.releaseDate,
-    duration: req.body.duration,
     title: req.body.title,
-    description: req.body.description,
-    weather: req.body.weather,
-    actors: req.body.actors,
-    writers: req.body.writers,
   };
+  const database = await pool.connect();
+  // todo: put column names into insert statements
+  await database.query(
+    `INSERT INTO episodes VALUES ('${newEpisode.episodeNumber}', '${newEpisode.title}')`
+  );
   res.json(newEpisode);
 });
 
-app.put("/rest-api/episodes-by-year", (req, res) => {
+app.put("/rest-api/episodes-by-year", async (req, res) => {
   let episodeList = episodes.filter(
     (episode) => parseInt(episode.releaseDate.substring(0, 4)) === req.body.year
   );
   res.json(episodeList);
 });
 
-app.delete("/rest-api/episodes", (req, res) => {
-  episodes.splice(req.body.id, 1);
+app.delete("/rest-api/episodes", async (req, res) => {
+  const database = await pool.connect();
+  await database.query(
+    `DELETE FROM episodes WHERE (${episodeNumber}='${req.episodeNumber}')`
+  );
   res.status(200);
   res.json("The episode has been deleted.");
 });
