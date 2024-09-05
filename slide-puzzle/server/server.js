1372bd6c25518a85722c3833b6270f245b0747b2
Thane <tmoonder@gmail.com>
2024-09-04 20:29:11 -0400

    Server now fetches from random image api.

--- slide-puzzle/server/server.js
+++ slide-puzzle/server/server.js
@@ -7,12 +7,15 @@ app.use(cors());
 app.use(express());
 
 app.get("/", (req, res) => {
-  const url = "";
+  const url = "https://api.api-ninjas.com/v1/randomimage";
+  const API_KEY = process.env.API_KEY;
   console.log(url);
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      res.send({ data });
+  fetch(url, { headers: { "X-Api-Key": API_KEY, "Accept": "image/jpg" }})
+    .then((res) => res.blob())
+    .then((blob) => {
+      console.log(blob);
+      // const imageUrl = URL.createObjectURL(blob);
+      res.send({ blob });
     })
     .catch((err) => {
       console.log(err);
