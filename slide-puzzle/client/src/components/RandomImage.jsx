cc701a9472fb6c74e169090cc9e6b7bd14888553
Thane <tmoonder@gmail.com>
2024-09-05 10:11:57 -0400

    Added puzzle component to random image component.

--- slide-puzzle/client/src/components/RandomImage.jsx
+++ slide-puzzle/client/src/components/RandomImage.jsx
@@ -5,6 +5,24 @@ function RandomImage() {
   const [imageUrl, setImageUrl] = useState("");
   const [imageAlt, setImageAlt] = useState("");
   const [category, setCategory] = useState("");
+  let testObject = {
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -38,7 +56,24 @@ function RandomImage() {
         width="500"
         style={{ border: "thin solid black" }}
       ></img>
-      <Puzzle />
+      <Puzzle
+        row0Column0={testObject[0]}
+        row0Column1={testObject[1]}
+        row0Column2={testObject[2]}
+        row0Column3={testObject[3]}
+        row1Column0={testObject[4]}
+        row1Column1={testObject[5]}
+        row1Column2={testObject[6]}
+        row1Column3={testObject[7]}
+        row2Column0={testObject[8]}
+        row2Column1={testObject[9]}
+        row2Column2={testObject[10]}
+        row2Column3={testObject[11]}
+        row3Column0={testObject[12]}
+        row3Column1={testObject[13]}
+        row3Column2={testObject[14]}
+        row03olumn3={testObject[15]}
+      />
       <form onSubmit={handleSubmit}>
         <input
           list="categories"
